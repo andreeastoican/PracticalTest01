fc730c3e33b24e0a78952a4c4568f226b74834a4
Andreea Stoican <stoican_and@yahoo.com>
2015-03-31 10:42:32 +0300

    v2

--- PracticalTest01/src/ro/pub/cs/systems/pdsd/practicaltest01/PracticalTest01MainActivity.java
+++ PracticalTest01/src/ro/pub/cs/systems/pdsd/practicaltest01/PracticalTest01MainActivity.java
@@ -2,16 +2,99 @@ package ro.pub.cs.systems.pdsd.practicaltest01;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class PracticalTest01MainActivity extends Activity {
 
+	String name="";
+    String phone="";
+    String email="";
+    int press=0;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_main);
+        
+        Button buttonName= (Button) findViewById(R.id.button_name);
+        Button buttonEmail= (Button) findViewById(R.id.button_email);
+        Button buttonPhone= (Button) findViewById(R.id.button_phone);
+        
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        
+        buttonName.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				press=1;
+				if(name=="") {
+					editText.setHint("Name");
+				} else {
+					editText.setText(name);
+				}
+			}
+		});
+        
+        buttonEmail.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				press=2;
+				if(email=="") {
+					editText.setHint("Email");
+				} else {
+					editText.setText(email);
+				}
+			}
+		});
+        
+        buttonPhone.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				press=3;
+				if(phone=="") {
+					editText.setHint("Phone");
+				} else {
+					editText.setText(phone);
+				}
+			}
+		});
+        
+        editText.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if(press==1) {
+					name=editText.getText().toString();
+				} else if(press==2) {
+					email=editText.getText().toString();
+				} else if(press==3) {
+					phone=editText.getText().toString();
+				}
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		})
+        
     }
 
 
